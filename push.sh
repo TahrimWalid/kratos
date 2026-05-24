cf2b3572bf14398bd73ac0167460052c52d101fe
TahrimWalid <tahrimwalid@gmail.com>
2026-05-24 23:20:41 +0000

    chore: remove generated docs, logs and scripts from repo (now ignored)

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Quick sync to CSC VM for testing
-
-rsync -avz --exclude='venv/' --exclude='.venv/' --exclude='__pycache__/' --exclude='*.pyc' --exclude='.git/' \
-  /home/walid000/kratos/ ubuntu@86.50.169.179:~/kratos/
-
-echo "[PUSH] Synced to CSC VM"
